6ba6153222615a503d2d431cb5ec7fba567e3074
Simon Huang <simon.huang@telecom-st-etienne.fr>
2026-04-24 11:45:48 +0200

    Add prefix_sum

--- lessons/prefix_sum/prefix_sum.py
+++ lessons/prefix_sum/prefix_sum.py
@@ -7,3 +7,11 @@ def prefix_sum(nums):
 
 # To calculate the sum of a subarray between indexes i and j
 # prefix[j + 1] - prefix[i]
+
+# 2nd Implementation
+def prefix_sum(nums):
+    n = len(nums)
+    prefix_sum = [0]
+    for i, num in enumerate(nums):
+        prefix_sum.append(prefix_sum[-1] + num)
+    return prefix_sum
